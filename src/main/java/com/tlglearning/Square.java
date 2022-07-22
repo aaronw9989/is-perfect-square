6790301079c831a5a81c02900483d8001a2ee889
aaronw9989 <williamsa493@strose.edu>
2022-07-22 17:57:39 -0500

    Added binary search algorithm

--- src/main/java/com/tlglearning/Square.java
+++ src/main/java/com/tlglearning/Square.java
@@ -22,13 +22,55 @@ public final class Square {
    * @return Flag indicating whether {@code input} is a perfect square or not.
    * @throws IllegalArgumentException When {@code input} is negative.
    */
+
+
+  /*
+   * NOTE: Max int value in Java = 2147483647
+   * NOTE: Max long value in Java = 9,223,372,036,854,775,807
+   * NOTE: Max double size in Java = 10^308
+   *
+   * Math.sqrt() is preferred, it over Math.pow()
+   *
+   */
   public static boolean isPerfectSquare(long input) throws IllegalArgumentException {
+    
+    boolean result = false; 
+    
     if(input < 0) {
       throw new IllegalArgumentException();
+    } else if (input == 0 || input == 1) {
+      return true;
     }
 
+    /*
+        input = 9
 
-    throw new UnsupportedOperationException("Not yet implemented"); // TODO Replace with implementation.
+        low = 3
+        high = 3
+        middle = 3
+     */
+
+
+    long low = 1;
+    long high = input;
+    long middle = 0;
+
+    while(low <= high) {
+
+      middle = (low + high) / 2;
+
+      if(middle * middle == input) {
+        return true;
+      } else if (middle * middle < input) {
+        low = middle + 1;
+      } else {
+        high = middle - 1;
+      }
+
+    }
+
+    return false;
+    // throw new UnsupportedOperationException("Not yet implemented"); // TODO Replace with implementation.
   }
 
 }
